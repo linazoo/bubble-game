c10766914d6634c3ba11d4653c78a1cd247547e6
linazoo <linazooper@gmail.com>
2018-05-06 20:38:48 -0500

    added game container - hooked up container to redux store which i also made

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import Game from './containers/Game';
-import reducer from './reducers';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import './index.css';
+import Game from './containers/Game';
+import reducer from './reducers';
+import registerServiceWorker from './registerServiceWorker';
 
+/* eslint-disable no-underscore-dangle */
 const store = createStore(
-  reducer, /* preloadedState, */
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    reducer, /* preloadedState, */
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
+/* eslint-enable */
 
 ReactDOM.render(
-  <Provider store={ store }>
-    <Game />
-  </Provider>,
-  document.getElementById('root'),
+    <Provider store={store}>
+        <Game />
+    </Provider>,
+    document.getElementById('root'),
 );
-
 registerServiceWorker();
