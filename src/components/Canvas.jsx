aa57785c29e8dc877afa36a2983f31d96ec4b326
linazoo <linazooper@gmail.com>
2018-05-10 22:59:54 -0500

    added a font for the score board and added a current socre component

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -5,6 +5,7 @@ import Sky from './Sky';
 import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
+import CurrentScore from './CurrentScore';
 
 const Canvas = (props) => {
   const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
@@ -15,10 +16,17 @@ const Canvas = (props) => {
       onMouseMove={props.trackMouse}
       viewBox={viewBox}
     >
+      <defs>
+        <filter id="shadow">
+          <feDropShadow dx="1" dy="1" stdDeviation="2" />
+        </filter>
+      </defs>
+
       <Sky />
       <Ground />
       <CannonPipe rotation={props.angle} />
       <CannonBase />
+      <CurrentScore score={15} />
     </svg>
   );
 };
