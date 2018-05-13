f8d9202a01b5d65dbf47261c746a92ab852cae18
linazoo <linazooper@gmail.com>
2018-05-12 20:56:53 -0500

    created most of the elements i need to complete the game (using react, redux, and svg

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,7 +6,7 @@ export const createInterval = 1000;
 
 export const maxFlyingObjects = 4;
 
-export const flyingObjectsStarterYAxis = -1000;
+export const flyingObjectsStarterYAxis = -1100;
 
 export const flyingObjectsStarterPositions = [
   -300,
@@ -14,3 +14,5 @@ export const flyingObjectsStarterPositions = [
   150,
   300,
 ];
+
+export const gameHeight = 1200;
