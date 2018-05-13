5cfe35e04bad1d65b52358ef716342c38617222d
linazoo <linazooper@gmail.com>
2018-05-12 20:07:47 -0500

    started creating the Start Game button component

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,3 +4,11 @@ export const moveObjects = mousePosition => ({
   type: MOVE_OBJECTS,
   mousePosition,
 });
+
+export const START_GAME = 'START_GAME';
+
+// ... moveObjects
+
+export const startGame = () => ({
+  type: START_GAME,
+});
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,11 +1,10 @@
-/*  <circle cx={0} cy={0} r={50} /> */
 import React from 'react';
 import PropTypes from 'prop-types';
 import Sky from './Sky';
 import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
-import CurrentScore from './CurrentScore';
+import CurrentScore from './CurrentScore'
 import FlyingObject from './FlyingObject';
 import StartGame from './StartGame';
 
@@ -24,22 +23,37 @@ const Canvas = (props) => {
           <feDropShadow dx="1" dy="1" stdDeviation="2" />
         </filter>
       </defs>
-
       <Sky />
       <Ground />
       <CannonPipe rotation={props.angle} />
       <CannonBase />
       <CurrentScore score={15} />
-      <FlyingObject position={{x: -150, y: -300}}/>
-      <FlyingObject position={{x: 150, y: -300}}/>
-      <StartGame onClick={() => console.log('Aliens, Go Home!')} />
+
+      { ! props.gameState.started &&
+        <g>
+          <StartGame onClick={() => props.startGame()} />
+        </g>
+      }
+
+      { props.gameState.started &&
+        <g>
+          <FlyingObject position={{x: -150, y: -300}}/>
+          <FlyingObject position={{x: 150, y: -300}}/>
+        </g>
+      }
     </svg>
   );
 };
 
 Canvas.propTypes = {
   angle: PropTypes.number.isRequired,
+  gameState: PropTypes.shape({
+    started: PropTypes.bool.isRequired,
+    kills: PropTypes.number.isRequired,
+    // lives: PropTypes.number.isRequired,
+  }).isRequired,
   trackMouse: PropTypes.func.isRequired,
+  startGame: PropTypes.func.isRequired,
 };
 
 export default Canvas;
